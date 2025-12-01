004c521057547087fc5fb72dea2f071012837032
NurlanQadirov <nurlanqdirov4@icloud.com>
2025-12-01 16:15:53 +0400

    Vite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/storage'],
-          animations: ['framer-motion']
-        }
-      }
-    }
-  }
 });
